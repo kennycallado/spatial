b08b5cf0a58d9e76aaa1376322a323f6e6bcd3ac
kennycallado <kennycallado@hotmail.com>
2022-08-03 19:31:01 +0200

    UPDATE Actualiza health checker test - liveness.spec.ts

--- tests/functional/liveness.spec.ts
+++ tests/functional/liveness.spec.ts
@@ -1,8 +1,8 @@
 import { test } from '@japa/runner'
 
 test('Liveness probe test', async ({ client }) => {
-  const response = await client.get('/liveness')
+  const response = await client.get('health')
 
   response.assertStatus(200)
-  response.assertBody({ status: 'ok' })
+  response.assertBodyContains({ healthy: true })
 })
